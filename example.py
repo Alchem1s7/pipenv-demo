4d8eecf5b7ada9c0312993646299e918bcb21e1b
Daniel Lugo <daniel.lugo@ul.com>
2024-12-19 12:27:38 -0600

    added seaborn and changed the code

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sys
+import seaborn as sns 
 
 print("The python executable comes from:")
 print(sys.executable)
@@ -8,3 +9,4 @@ print("The libraries versions are:")
 print(pd.__version__)
 print(np.__version__)
 print("Hello world!")
+print("The version of seaborn is: ", sns.__version__)
