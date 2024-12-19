7fb955310013ebf0d0890c9acfe6e7f79a72a2db
Daniel Lugo <daniel.lugo@ul.com>
2024-12-19 08:56:45 -0600

    First commit, pipenv demo

---
+++ example.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import sys
+
+print("The python executable comes from:")
+print(sys.executable)
+print("The libraries versions are:")
+print(pd.__version__)
+print(np.__version__)
+print("Hello world!")
